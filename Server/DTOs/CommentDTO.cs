cbf067871127cf5cd049a8c24ba36f63b56ac160
Tran Trong Nhan <152136876+ttnhan227@users.noreply.github.com>
2025-05-18 03:07:28 +0700

    commit

--- Server/DTOs/CommentDTO.cs
+++ Server/DTOs/CommentDTO.cs
@@ -3,7 +3,7 @@ namespace Client.DTOs;
 public class CommentDTO
 {
     public int CommentID { get; set; }
-    public int VideoID { get; set; }
+    public string VideoID { get; set; }
     public int UserID { get; set; }
     public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
