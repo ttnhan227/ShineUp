5a45ad8d6763780492b4785f0631a7d0cb19a276
Tran Trong Nhan <152136876+ttnhan227@users.noreply.github.com>
2025-05-17 17:46:19 +0700

    Commit delete

--- Client/DTOs/LikeDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Client.DTOs;
-
-public class LikeDTO
-{
-    public int LikeID { get; set; }
-    public int VideoID { get; set; }
-    public int UserID { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
