00f1b18d88fd83eafdb99b520ccef8c1e13a331b
Tran Trong Nhan <152136876+ttnhan227@users.noreply.github.com>
2025-05-17 17:46:19 +0700

    Commit delete

--- Client/DTOs/CategoryDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Client.DTOs;
-
-public class CategoryDTO
-{
-    public int CategoryID { get; set; }
-    public string CategoryName { get; set; }
-    public string Description { get; set; }
-}
