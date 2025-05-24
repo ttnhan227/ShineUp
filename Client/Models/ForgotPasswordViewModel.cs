66d203c881a6fe77df5ac00dd59dce96866a5502
Tran Trong Nhan <152136876+ttnhan227@users.noreply.github.com>
2025-05-25 02:31:17 +0700

    reset password validations

--- Client/Models/ForgotPasswordViewModel.cs
+++ Client/Models/ForgotPasswordViewModel.cs
@@ -19,7 +19,16 @@ public class ResetPasswordViewModel
     [StringLength(6, MinimumLength = 6)]
     public string OTP { get; set; }
 
-    [Required]
-    [StringLength(100, MinimumLength = 6)]
+    [Required(ErrorMessage = "New Password is required")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "New Password must be at least 8 characters")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        ErrorMessage = "New Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")]
+    [DataType(DataType.Password)]
+    [Display(Name = "New Password")]
     public string NewPassword { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm New Password")]
+    [Compare("NewPassword", ErrorMessage = "The new password and confirmation new password do not match.")]
+    public string ConfirmNewPassword { get; set; }
 }
